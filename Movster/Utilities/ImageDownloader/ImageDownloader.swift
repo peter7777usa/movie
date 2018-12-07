712d77ad58e18ea6179d30d34cadd997ba824f93
Fong, Peter <pefong@paypal.com>
2018-12-07 02:59:22 -0800

    added download queue logics

--- Movster/Utilities/ImageDownloader/ImageDownloader.swift
+++ Movster/Utilities/ImageDownloader/ImageDownloader.swift
@@ -11,20 +11,44 @@ import UIKit
 class ImageDownloader: NSObject {
     static let sharedInstance = ImageDownloader()
     let imageCache = NSCache<NSString, UIImage>()
+    var ongoingDownloadQueue = [String: [(UIImage?) -> Void]]()
+    
+    // MARK: - Public Methods
     
     func downloadImage(imageURL: String, completion: @escaping (_ image: UIImage?) -> Void) {
-        guard let url = URL(string: imageURL) else { return }
-        if let cachedImage = imageCache.object(forKey: url.absoluteString as NSString) {
+        if let cachedImage = imageCache.object(forKey: imageURL as NSString) {
             completion(cachedImage)
         } else {
+            queueUpBlockWithURL(imageURL: imageURL, imageExecutionBlock: completion)
+        }
+    }
+    
+    // MARK: - Private Methods
+    
+    private func queueUpBlockWithURL(imageURL: String, imageExecutionBlock: @escaping (UIImage?) -> Void) {
+        if var blockArray = ongoingDownloadQueue[imageURL] {
+            blockArray.append(imageExecutionBlock)
+            ongoingDownloadQueue[imageURL] = blockArray
+        } else {
+            ongoingDownloadQueue[imageURL] = [imageExecutionBlock]
+            guard let url = URL(string: imageURL) else { return }
             URLSession.shared.dataTask(with: url) { [weak self] (data, response, error) in
                 if error != nil {
                     print("error")
                 }
                 guard let data = data, let retrievedImage = UIImage(data: data) else { return }
                 self?.imageCache.setObject(retrievedImage, forKey: url.absoluteString as NSString)
-                completion(retrievedImage)
+                self?.executeAllBlockGivenImageURL(imageURL: imageURL)
                 }.resume()
         }
     }
+    
+    private func executeAllBlockGivenImageURL(imageURL: String) {
+        if let blockArray = ongoingDownloadQueue[imageURL] {
+            for block in blockArray {
+                block(imageCache.object(forKey: imageURL as NSString))
+            }
+        }
+    }
 }
+
